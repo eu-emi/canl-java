6b00c778b51dd4aaa5aa4a4835724ea34235f6a1
Mattias Ellert <mattias.ellert@physics.uu.se>
2020-05-05 16:52:11 +0200

    The HTML tag <tt> is obsolete. Use the <code> tag instead. The error message from javadoc in JDK 11 is:
    error: tag not supported in the generated HTML version: tt

--- src/main/java/eu/emi/security/authn/x509/package-info.java
+++ src/main/java/eu/emi/security/authn/x509/package-info.java
@@ -1,6 +1,7 @@
 /**
  * Contains API of the library. The interfaces defined here are implemented
- * by the classes from the the <tt>eu.emi.security.authn.x509.impl</tt> package.
+ * by the classes from the the <code>eu.emi.security.authn.x509.impl</code>
+ * package.
  */
 package eu.emi.security.authn.x509;
 
